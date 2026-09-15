7aaa1bd6fe1a0de17c828fb80ba7e599571b5b8a
denyslandino-lab <denyslandino@gmail.com>
2026-09-14 22:11:49 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'pedidos-publicacoes-v1';
+const FILES = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
